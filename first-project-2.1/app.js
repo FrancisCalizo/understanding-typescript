9492dd0518d8a9a452538e6fd72ab03da2bf67bc
Francis Calizo <francisacalizo@gmail.com>
2020-05-14 06:37:29 -0400

    Finsh video 14

--- first-project-2.1/app.js
+++ first-project-2.1/app.js
@@ -1,8 +1,14 @@
-function add(n1, n2) {
-    console.log(typeof number1);
+function add(n1, n2, showResult) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error('Incorrect Input');
+    // }
+    if (showResult) {
+        console.log(resultPhrase + n1 + n2);
+    }
     return n1 + n2;
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is ';
+add(number1, number2, printResult);
--- first-project-2.1/app.ts
+++ first-project-2.1/app.ts
@@ -1,11 +1,17 @@
-function add(n1: number, n2: number) {
-  console.log(typeof number1);
-  return n1 + n2;
+function add(n1: number, n2: number, showResult: boolean) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('Incorrect Input');
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(resultPhrase + result);
+  }
+  return result;
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is ';
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult);
